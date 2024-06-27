1dcb2738dfafc0f427065891f203410af6f9f803
HuaiLing2006 <anime14124213@gmail.com>
2024-06-27 20:00:54 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="DemNguoc">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://www.facebook.com/l3RuanHuaiLing25072006/
 export default defineConfig({
   plugins: [react()],
   test: {
